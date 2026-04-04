8e9f44c157cd57b99a5df7a9006200773fcc7380
Henrique Buzin <cultura.henrique@gmail.com>
2026-04-03 21:50:41 -0300

    REMOVENDO CADDY

--- src/main.py
+++ src/main.py
@@ -159,4 +159,5 @@ def send():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=DEBUG)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=DEBUG)
